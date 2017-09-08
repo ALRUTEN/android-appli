d48f3e8c6a867c7134347968083bb3d4a0374bb1
ALRUTEN <laurentnavarro31@gmail.com>
2017-09-08 10:48:38 +0200

    modif checkbox

--- app/src/main/java/com/example/apprenti/premierequetestudio/MainActivity.java
+++ app/src/main/java/com/example/apprenti/premierequetestudio/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.apprenti.premierequetestudio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final CheckBox checkBox =(CheckBox) findViewById(R.id.checkbox);
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editFirstName = (EditText) findViewById(R.id.first_name);
+                EditText editLastName = (EditText) findViewById(R.id.last_name);
+                Button button = (Button) findViewById(R.id.button);
+                if (checkBox.isChecked()) {
+                    editFirstName.setEnabled(true);
+                    editLastName.setEnabled(true);
+                    button.setEnabled(true);
+
+                }else {
+                    editFirstName.setEnabled(false);
+                    editLastName.setEnabled(false);
+                    button.setEnabled(false);
+
+                }
+
+            }
+        });
+
     }
 }
