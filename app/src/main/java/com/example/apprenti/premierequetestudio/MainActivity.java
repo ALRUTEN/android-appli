bb469546ad3aa52676375109242e599541894527
ALRUTEN <laurentnavarro31@gmail.com>
2017-09-08 11:39:22 +0200

    html main.xml

--- app/src/main/java/com/example/apprenti/premierequetestudio/MainActivity.java
+++ app/src/main/java/com/example/apprenti/premierequetestudio/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.apprenti.premierequetestudio;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,6 +38,37 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editFirstName = (EditText) findViewById(R.id.first_name);
+                EditText editLastName = (EditText) findViewById(R.id.last_name);
+                if(editFirstName.getText().toString().isEmpty()
+                    ||editLastName.getText().toString().isEmpty()) {
+                    Context context = getApplicationContext();
+                    CharSequence text = getResources().getString(R.string.error);
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+
+                }else{
+
+                    TextView textCongratulation = (TextView) findViewById(R.id.congratulation);
+                    textCongratulation.setText(getResources().getString(R.string.congratulation)
+                            + " " + editFirstName.getText().toString()
+                             + " " + editLastName.getText().toString() +" !"
+
+                    );
+
+
+                }
+
+            }
+        });
+
 
     }
 }
